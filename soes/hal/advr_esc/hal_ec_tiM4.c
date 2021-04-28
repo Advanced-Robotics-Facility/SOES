2760534360e5d2d46d5387f4c9c90aec8d32761f
Alessio Margan <alessio.margan@iit.it>
2021-04-28 20:27:36 +0200

    [TI tm4c] unify pins name

--- soes/hal/advr_esc/hal_ec_tiM4.c
+++ soes/hal/advr_esc/hal_ec_tiM4.c
@@ -9,15 +9,14 @@
 
 #include <pins.h>
 
-void cs_up(void) { GPIOPinWrite(SPI_ECAT_SSI_PORT, SPI_ECAT_CS_PIN, SPI_ECAT_CS_PIN); }
-void cs_dn(void) { GPIOPinWrite(SPI_ECAT_SSI_PORT, SPI_ECAT_CS_PIN, 0); }
+void cs_up(void) { GPIOPinWrite(ECAT_SSI_GPIO_PORTBASE, ECAT_SSI_CS, ECAT_SSI_CS); }
+void cs_dn(void) { GPIOPinWrite(ECAT_SSI_GPIO_PORTBASE, ECAT_SSI_CS, 0); }
 
 
 uint8_t spi_write(uint8_t data) {
     uint32_t ret = 0;
-    SSIDataPut(SSI_ECAT_BASE, data);
-    while( SSIBusy(SSI_ECAT_BASE) ) { }
-    SSIDataGet(SSI_ECAT_BASE, &ret);
+    SSIDataPut(ECAT_SSI_BASE, data);
+    while( SSIBusy(ECAT_SSI_BASE) ) { }
+    SSIDataGet(ECAT_SSI_BASE, &ret);
     return (uint8_t)ret;
 }
-
