2c39ede16ea04be84d8d12c9e5c9304c6c4d4941
Margan Alessio <alessio.margan@iit.it>
2024-10-10 16:12:49 +0200

    [stm32][et1100]
    DISCO +  ET1100 phil

--- soes/hal/advr_esc/esc_hw_et1100.c
+++ soes/hal/advr_esc/esc_hw_et1100.c
@@ -6,13 +6,37 @@
 #define ESC_CMD_WRITE   0x04
 #define ESC_CMD_ADDREX  0x06
 #define ESC_CMD_NOP     0x00
-#define ESC_TERM        0xff
+#define ESC_TERM     	0xFF
 #define ESC_NEXT        0x00
+#define ESC_WAIT     	0xFF
 
+static uint32_t alevent_spi_vec[256] = {0};
+static uint32_t alevent_reg_vec[256] = {0};
+static uint8_t 	alevent_idx = 0;
+
+
+inline static uint32_t ESC_Read_ALEVENT(void) {
+
+	register uint32_t alevent;
+
+	cs_dn();
+	// address phase
+	spi_write((uint8_t)(ESCREG_ALEVENT>>5));
+	spi_write((uint8_t)(((ESCREG_ALEVENT&0x1F)<<3)|ESC_CMD_READWS));
+	// due to ESC_CMD_READWS cmd ... tx wait state
+	spi_write(ESC_WAIT);
+	// data
+	alevent  = spi_write(ESC_NEXT);
+	alevent |= spi_write(ESC_NEXT) << 8;
+	alevent |= spi_write(ESC_NEXT) << 16;
+	alevent |= spi_write(ESC_TERM) << 24;
+	cs_up();
+
+	return htoel(alevent);
+}
 
 
 /**
- * 
  * 
  * @author amargan (9/27/2013)
  * 
@@ -52,26 +76,50 @@ inline static uint16_t addrPhase(uint16_t addr, uint8_t cmd) {
  */
 void ESC_read(uint16_t addr, void * data, uint16_t len) {
 
-    uint8_t     i=0;
-    
-    cs_dn();
-    // address phase
-#ifdef STM32
-    addrPhase(addr, ESC_CMD_READWS);
-#else
-    ESCvar.ALevent = addrPhase(addr, ESC_CMD_READWS);
-#endif
-    spi_write(0xFF);
+	uint8_t i = 0;
 
+	cs_dn();
+	// Write address and command to device
+	ESCvar.ALevent = addrPhase(addr, ESC_CMD_READWS);
+	alevent_spi_vec[alevent_idx] = ESCvar.ALevent;
+	/* Between the last address phase byte and the first data byte of a read access, the SPI master has to
+       wait for the SPI slave to fetch the read data internally. Subsequent read data bytes are prefetched
+       automatically, so no further wait states are necessary.
+       The SPI master can choose between these possibilities:
+		  - The SPI master may either wait for the specified worst case internal read time tread after the last
+			  address/command byte and before the first clock cycle of the data phase.
+		  - The SPI master inserts one Wait State byte after the last address/command byte. The Wait State
+			byte must have a value of 0xFF transferred on SPI_DI.
+			spi_write(0xFF);
+	*/
+    spi_write(ESC_WAIT);
+    /* Here we want to read data and keep MOSI low (0x00) during
+	 * all bytes except the last one where we want to pull it high (0xFF).
+	 * Read (and write termination bytes).
+	 */
     // data phase
     for ( i=0;i<len-1;i++ ) {
-        *(((uint8_t*)data)+i) = spi_write(0x00);
+        *(((uint8_t*)data)+i) = spi_write(ESC_NEXT);
     }
     // read termination byte
-    *(((uint8_t*)data)+i) = spi_write(0xFF);
-    
+    *(((uint8_t*)data)+i) = spi_write(ESC_TERM);
+
     cs_up();
 
+#ifdef STM32
+    ESCvar.ALevent = ESC_Read_ALEVENT();
+#endif
+#if 0
+    alevent_reg_vec[alevent_idx] = ESCvar.ALevent;
+    alevent_idx++;
+    if ( alevent_idx >= 10 ) {
+    	while (alevent_idx--) {
+    		DPRINT ("%d 0x%" PRIX32 " 0x%" PRIX32 "\n",
+    				alevent_idx, alevent_spi_vec[alevent_idx], alevent_reg_vec[alevent_idx]);
+    	}
+    	DPRINT ("\n");
+    }
+#endif
     return;
 }
 
@@ -88,23 +136,19 @@ void ESC_read(uint16_t addr, void * data, uint16_t len) {
  */
 void ESC_write(uint16_t addr, void * data, uint16_t len) {
 
-    uint8_t     i=0;
-    
-    cs_dn();
-    // address phase
-#ifdef STM32
-    addrPhase(addr, ESC_CMD_WRITE);
-#else
-    ESCvar.ALevent = addrPhase(addr, ESC_CMD_WRITE);
-#endif
+	uint8_t i = 0;
 
+	cs_dn();
+    // Write address and command to device
+    ESCvar.ALevent = addrPhase(addr, ESC_CMD_WRITE);
     // data phase
     for ( i=0;i<len;i++ ) {
         spi_write(*(((uint8_t*)data)+i));
-    }       
-    
+    }
     cs_up();
-        
+#ifdef STM32
+    ESCvar.ALevent = ESC_Read_ALEVENT();
+#endif
     return;
 }
 
